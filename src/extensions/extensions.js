c6d2af79e83b8a3d39db7f36c3ec0960c30b8e20
Atakan Temizkan <atikotemizkan@gmail.com>
2024-04-29 16:31:25 +0300

    commit for other computer

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,7 +4,15 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
-
+function timerStatus(minutesRemaining) {
+  if (minutesRemaining === 0) {
+    return "Phil's cake is ready!"
+  } else if (minutesRemaining > 0) {
+    return 'The cake is still baking!'
+  } else {
+    return "You didn't set a timer!"
+  }
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,7 +21,13 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
-
+function estimatePrepTime(ingredients, ptp) {
+  if (ptp !== undefined) {
+    return ingredients.length * ptp
+  } else {
+    return ingredients.length * 2
+  }
+}
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
 // - a list of ingredients
@@ -29,7 +43,16 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
-
+function calculateQuantities(ingredients, layers) {
+  const objekt = { sugar: 0, eggs: 0 }
+  if (ingredients.includes('sugar')) {
+    objekt.sugar = 100 * layers
+  }
+  if (ingredients.includes('eggs')) {
+    objekt.eggs = 2 * layers
+  }
+  return objekt
+}
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,7 +65,11 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+function improveRecipe(takenObject, portion) {
+  let newObject = objekt
+  newObject.(Object.keys(takenObject)[0]) = Object.values(takenObject)[0] * portion
+  return newObjekt
+}
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
---
+++ test.js
@@ -0,0 +1,10 @@
+let Allah = {
+  bbc: 'AA',
+  nbc: 'BB'
+}
+function improveRecipe(objekt, portion) {
+  let newObjekt
+  Object.values(newObjekt)[0] = Object.values(objekt)[0] + 'benis'
+  return objekt
+}
+console.log(improveRecipe(Allah))
